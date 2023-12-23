400a7e732afb930b485e2a6272eb4aa6b9d83f63
nima <nima.akbarzadeh@mail.mcgill.ca>
2023-12-23 17:44:50 -0500

    Minor updates in comments.

--- DL/datasets.py
+++ DL/datasets.py
@@ -1,9 +1,13 @@
+import torch
 from torchvision.datasets import MNIST, CIFAR10
 import torchvision.transforms as transforms
+import pandas as pd
+from torch.utils.data import TensorDataset
+from torch.utils.data.dataset import random_split
+from math import ceil
 
 
-def load_classification_data(data_name, data_info=True):
-    train_dataset, test_dataset, class_labels = None, None, None
+def load_classification_data(data_name, device=torch.device('cpu'), data_info=True):
     if data_name == 'mnist':
         # MNIST dataset
         # Transform the dataset to Tensors of normalized range [-1, 1]
@@ -20,6 +24,8 @@ def load_classification_data(data_name, data_info=True):
                              transform=transform)
         class_labels = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
 
+        return train_dataset, test_dataset, class_labels
+
     if data_name == 'cifar10':
         # CIFAR10 dataset
         # Transform the dataset to Tensors of normalized range [-1, 1]
@@ -35,4 +41,5 @@ def load_classification_data(data_name, data_info=True):
                                transform=transform)
         class_labels = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
-    return train_dataset, test_dataset, class_labels
+        return train_dataset, test_dataset, class_labels
+
