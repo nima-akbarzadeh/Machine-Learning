7b64b21134b99eef6914f07ebd712066b19da153
nima <nima.akbarzadeh@mail.mcgill.ca>
2023-12-25 20:43:29 -0500

    A test example for lunar landing with a random policy.

---
+++ RL/test.py
@@ -0,0 +1,13 @@
+import gym
+
+if __name__ == '__main__':
+
+    env = gym.make("LunarLander-v2", render_mode="human")
+    observation, info = env.reset(seed=42)
+    for _ in range(1000):
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+    env.close()
